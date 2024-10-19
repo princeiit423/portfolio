3bf3acac5cf0815cea89cb8b3d18f89d37fed2b7
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2024-10-19 16:38:11 +0530

    banner fix

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,58 +1,14 @@
 //import starImage from "./assets/star.svg";
 const Testimonial = () => {
   return (
-    <div className="pt-9 flex justify-center items-center h-screen overflow-hidden">
-      {/* Box with negative rotation */}
-      <div className="box flex flex-row gap-4 items-center bg-gradient-to-r from-green-400 to-cyan-400 w-auto h-24 transform -rotate-12 relative">
-        <h2 className="text-black text-xl font-bold">Accessible</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Secure</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Accessible</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Secure</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Accessible</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Secure</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Accessible</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-        <h2 className="text-black text-xl font-bold">Secure</h2>
-        <img
-          className="w-10 h-10"
-          src=""
-          alt="icon"
-        />
-      </div>
+    <div className="bg-gradient-to-r from-green-400 to-cyan-400 mt-20 mb-20">
+    <div className="banner flex flex-row h-20 justify-between overflow-x-scroll overflow-y-hidden no-scrollbar text-nowrap text-black items-center text-xl font-semibold">
+      <p className="p-5">🌟WebApp</p>
+      <p className="p-5">🌟OperSource</p>
+      <p className="p-5">🌟Project</p>
+      <p className="p-5">🌟MERN</p>
+      <p className="p-5">🌟Developer</p>
+    </div>
     </div>
   );
 };
